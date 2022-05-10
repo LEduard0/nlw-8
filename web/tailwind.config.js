5d4da75b2b9d8353a7f5157d31a937f4a85e21c4
Luiz Eduardo <leduardo152@gmail.com>
2022-05-10 11:05:07 -0300

    add buttons widget form

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,17 @@
+const { borderRadius } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
       colors: {
         brand: {
+          300: "#996DFF",
           500: "#8257E5"
         }
+      },
+      borderRadius: {
+        md: '4px'
       }
     },
   },
